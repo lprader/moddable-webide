c46fce44b535ab739fa01c6f06436824e34c8bab
Lizzie Prader <lizzie@moddable.tech>
2019-05-30 21:41:12 -0700

    load gists automatically

--- src/overmind/onInitialize.ts
+++ src/overmind/onInitialize.ts
@@ -7,14 +7,18 @@ export const onInitialize: OnInitialize = async ({
 }) => {
   actions.Compiler.load();
   const urlParams = new URLSearchParams(window.location.search);
-  await actions.Storage.openProject('light');
-  await actions.importGist('c2e638a2e8ebaff796d67f2d2dd0783d');
-  await actions.Storage.openProject('sign');
-  await actions.importGist('9f526d43030bd3c96fead27eebaf5303');
-  await actions.Storage.openProject('thermometer');
-  await actions.importGist('514a010e7acff8520f60839ecc749da5');
-  const projectName = urlParams.get('light');
+  const projectName = urlParams.get('project');
   if (await effects.Storage.hasProject(projectName)) {
     actions.Storage.openProject(projectName);
+  } else if (await effects.Storage.hasProject('light')) {
+	actions.Storage.openProject('light');
+  } else {
+	await actions.Storage.openProject('light');
+	await actions.importGist('c2e638a2e8ebaff796d67f2d2dd0783d');
+	await actions.Storage.openProject('sign');
+	await actions.importGist('9f526d43030bd3c96fead27eebaf5303');
+	await actions.Storage.openProject('thermometer');
+	await actions.importGist('514a010e7acff8520f60839ecc749da5');
+	actions.Storage.openProject('light');
   }
 };
