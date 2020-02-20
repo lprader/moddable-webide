34a02824affe14f7627f535a93db433956a9d7ee
Lizzie Prader <lizzie@moddable.tech>
2020-02-19 16:19:03 -0800

    change name of colored-circles app

--- src/overmind/onInitialize.ts
+++ src/overmind/onInitialize.ts
@@ -15,7 +15,7 @@ export const onInitialize: OnInitialize = async ({
   } else {
 	await actions.Storage.openProject('time-date-weather');
 	await actions.importGist('68f581c1067a1d71ad7e2f7f5a7bdfec');
-	await actions.Storage.openProject('colored-circles');
+	await actions.Storage.openProject('colored-squares');
 	await actions.importGist('1abbd984db8b087db14b49e143d40480');
 	await actions.Storage.openProject('motion-sensor');
 	await actions.importGist('a38d385727d03291d892938c59ab359c');
